b0d6d41dea57cd8109e4a9635dd52c819dca88b9
MANUEL <velazquez@garagescript.org>
2018-05-31 18:07:29 -0700

    solvedproblem9.js

--- curriculum/js0/9.js
+++ curriculum/js0/9.js
@@ -1,14 +1,17 @@
 /**
  * Takes in 2 numbers, return a function that
  *    takes in a number and returns the sum of all 3 numbers when called
- * @param {number} a
+10gg * @param {number} a
  * @param {number} b
  * @returns {function}
  */
 
 const solution = (a, b) => {
-  return () => {};
+  return (c) =>{
+    return a+b+c;
+  }
 };
+
 
 module.exports = {
   solution,

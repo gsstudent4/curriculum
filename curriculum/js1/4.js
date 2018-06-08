ab4c118f69fdc73f0ad0fb389e18fec998c6b312
MANUEL <velazquez@garagescript.org>
2018-06-07 18:59:55 -0700

    solvedproblem4jsonjs1

--- curriculum/js1/4.js
+++ curriculum/js1/4.js
@@ -6,8 +6,10 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (n, string, i = 0, result = "") => {
+  if (n === i) return result;
+  result = result + string;
+  return solution (n, string, i + 1, result);
 };
 
 module.exports = {
